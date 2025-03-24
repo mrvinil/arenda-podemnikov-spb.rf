4fac53230ddf338d1cd6c06a767e7ca9ff4bb448
mrvinil-test <lass.tx@yandex.ru>
2025-03-24 19:56:24 +0300

    test3

--- catalog/detail.php
+++ catalog/detail.php
@@ -1,6 +1,6 @@
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php"); ?>
 
-рпварапр
+рпварапрhfghf
 <?$APPLICATION->IncludeComponent("bitrix:news.detail", "catalog", Array(
 		"DISPLAY_DATE" => "Y",
 		"DISPLAY_NAME" => "Y",
